5ea85d7626ca2138cc0b6bdbcc4044cb186419dd
jsdevtom <contact@jsdevtom.com>
2019-04-15 08:30:00 +0200

    Handling Traffic with Ingress Controllers: Updating the Deployment Script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,25 @@
 
 IMAGES=( client server worker )
 CONTAINER_PREFIX=eu.gcr.io/docker-kubernetes-237606/jsdevtom/multi-
+GIT_SHA=$(git rev-parse HEAD)
 
-for i in "${IMAGES[@]}"
+for i in ${IMAGES[@]}
 do
-	docker build -t ${CONTAINER_PREFIX}${i} -f ./${i}/Dockerfile ./${i}
-    docker push ${CONTAINER_PREFIX}${i}
+	docker build \
+	    -t ${CONTAINER_PREFIX}${i}:latest \
+	    -t ${CONTAINER_PREFIX}${i}:${GIT_SHA} \
+	    -f ./${i}/Dockerfile ./${i}
+    docker push ${CONTAINER_PREFIX}${i}:latest
+    docker push ${CONTAINER_PREFIX}${i}:${GIT_SHA}
 done
 
 kubectl apply -f k8s
-kubectl set image deployment/server-deployment server=eu.gcr.io/docker-kubernetes-237606/jsdevtom/multi-server
+
+for i in ${IMAGES[@]}
+do
+    kubectl set image \
+    deployment/${i}-deployment \
+    ${i}=${CONTAINER_PREFIX}${i}:${GIT_SHA}
+done
+
+
